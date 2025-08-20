653648e0a26dc8108c7a39205b6320d0c5b83809
MENGKHEY KHORN <mengkheykhorn@MENGKHEYs-MacBook-Air.local>
2025-08-20 22:52:45 +0700

    fix eror at next.config.mjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@
 import type { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { TaskProvider } from "../components/taskProvider";
+import { TaskProvider } from "../components/TaskProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
